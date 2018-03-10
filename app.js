301d2e1685a2ad1ee85cd44510bc585057312992
Yi-Ching <31042652+TheProudSoul@users.noreply.github.com>
2018-03-11 01:56:11 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const routes = require('./routes');
+const mongooes = require('mongoose')
 const app = express();
+//const dbUrl = process.env.MONGOHQ_URL || 'mongodb://@localhost:27017/blog'
+
+//连接数据库
+//const db = mongooes.connect(dbUrl);
+
+//app.use(bodyParser());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post('/v1/app/language/translate', routes.translate.ts)
+app.post('/v1/app/language/speech', routes.translate.tts)
 
 const server = app.listen(3000, function () {
   const host = server.address().address;
