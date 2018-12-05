6cc6fbe4b6b0a54bc1744d9e895f95e742077ba2
delbertbeta <delbertbeta@live.com>
2018-12-05 23:24:33 +0800

    Fixed a typo.

--- routes/app/food/recommend/user.js
+++ routes/app/food/recommend/user.js
@@ -3,9 +3,10 @@ const { Food } = require.main.require('./db/model')
 const axios = require('axios')
 
 const handle = async (req, res) => {
-  const result = await axios.post(global.ml.url + '/v1/backend/food/recommend/id', {
-    id: req.body.userId
-  })
+  const data = {
+    id: req.body.id
+  }
+  const result = await axios.post(global.ml.url + '/v1/backend/food/recommend/id', data)
   const foods = await Promise.all(result.data.foods.map(async v => {
     let obj = JSON.parse(JSON.stringify(await Food.findOne({
       _id: v
